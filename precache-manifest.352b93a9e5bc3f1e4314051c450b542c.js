9d72b51af6c43ae8c8547ef8a0a37f67723af772
Rudenko Artem <artem.rud@bk.ru>
2019-11-04 10:46:26 +0200

    Updates

---
+++ precache-manifest.352b93a9e5bc3f1e4314051c450b542c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97668c2c9a91a72eaa9fc5cf7890fe5",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "65fe51c50b3bdfc5532f",
+    "url": "/react_add-todo-form/static/css/main.5d3bdd7c.chunk.css"
+  },
+  {
+    "revision": "1b2cfe77f2bb9f146d39",
+    "url": "/react_add-todo-form/static/js/2.3c77df80.chunk.js"
+  },
+  {
+    "revision": "65fe51c50b3bdfc5532f",
+    "url": "/react_add-todo-form/static/js/main.ce74ec53.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
